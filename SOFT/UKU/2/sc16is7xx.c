c0c8ec5922b07f9e70f935889b91b90fd726963a
pal73spa <pal@ngs.ru>
2018-04-25 11:44:05 +0700

    1234

--- SOFT/UKU/2/sc16is7xx.c
+++ SOFT/UKU/2/sc16is7xx.c
@@ -91,8 +91,8 @@ void sc16is700_init(uint32_t baudrate)
 
 unsigned char baud_h,baud_l;
 
-baud_h = (char)((10000000U/16U/baudrate)>>8);
-baud_l = (char)((10000000U/16U/baudrate)); 
+baud_h = (char)((50000000U/16U/baudrate)>>8);
+baud_l = (char)((50000000U/16U/baudrate)); 
 
 sc16is700_wr_byte(CS16IS7xx_LCR, 0x80);
 sc16is700_wr_byte(CS16IS7xx_DLL, baud_l);
